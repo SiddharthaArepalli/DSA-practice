2556c47557c485695a6a05bb555b1bbb67a64394
SiddharthaArepalli <asiddharthagoud@gmail.com>
2024-04-15 00:36:52 +0530

    some errors founded

--- Doubly_LinkedList/DL1.java
+++ Doubly_LinkedList/DL1.java
@@ -64,30 +64,66 @@ public class DL1 {
     }
 
     // deleting tail 
-    private static Node DeleteTail(Node head){
-        if(head == null || head.next == null){
-            return null;
+    // private static Node DeleteTail(Node head){
+    //     if(head == null || head.next == null){
+    //         return null;
+    //     }
+        
+    //     Node tail = head;
+    //     while(tail.next.next != null){ // Change condition here
+    //         tail = tail.next;
+    //     }
+        
+    //     Node back = tail;
+    //     tail = tail.next;
+    //     back.next = null; // Severing the link between back and tail
+    //     tail.prev = null;
+        
+    //     return head;
+    // }
+    private static Node DeleteK(Node head, int k) {
+        if (head == null || k <= 0) // Check for invalid inputs
+            return head;
+    
+        Node temp = head;
+        int count = 1; // Start from 1 because we are deleting the kth node
+    
+        while (temp != null && count < k) {
+            temp = temp.next;
+            count++;
         }
-        
-        Node tail = head;
-        while(tail.next.next != null){ // Change condition here
-            tail = tail.next;
+    
+        if (temp == null || temp.next == null) // If k is greater than the number of nodes
+            return head;
+    
+        Node back = temp.prev;
+        Node front = temp.next;
+    
+        if (back == null) { // If temp is the head node
+            front.prev = null;
+            return front;
+        } else {
+            back.next = front;
+            if (front != null) // If temp is not the last node
+                front.prev = back;
         }
-        
-        Node back = tail;
-        tail = tail.next;
-        back.next = null; // Severing the link between back and tail
-        tail.prev = null;
-        
+    
+        temp.prev = null;
+        temp.next = null;
         return head;
     }
     
+
     public static void main(String[] args) {
         
         int arr[] = {1,2,3,4,5,6};
         Node head = ArrayToDll(arr);
         // head = DeleteHead(head);
-        head = DeleteTail(head);
+        // head = DeleteTail(head);
+        head = DeleteK(head, 3);
         print(head);
+        
     }
+
+
 }
